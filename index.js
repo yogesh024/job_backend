1f5c6e99cee3f6d7a01a19888efa0b37b6f2353b
yogesh024 <ykajay55@gmail.com>
2024-08-02 01:26:17 +0530

    update

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173", // your frontend domain
+    origin: "https://mern-job-frontend-ymuw.vercel.app/", // your frontend domain
     credentials: true
   }));
 app.use(cookieParser());
